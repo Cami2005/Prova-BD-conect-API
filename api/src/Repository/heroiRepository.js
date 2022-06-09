4134d474f5bdbfaf1b0bcf88d668a50bcbe2d60a
Camile <camilevitoriarosa2005@gmail.com>
2022-06-08 22:01:33 -0300

    oi

--- api/src/Repository/heroiRepository.js
+++
@@ -1,9 +0,0 @@
-import { con } from "./conection.js";
-
-export async function SuperHeroi (heroi) {
-   const comando= ` insert into tb_heroi (nm_heroi , ds_superpoder , bt_voa)
-values ( ?,?, ?)`
-    const[resposta]= await con.query (comando, [heroi.nome, heroi.superpoderes, heroi.voa])
-    return resposta;
-
-}
